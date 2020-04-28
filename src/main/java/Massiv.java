19b312d1838122ed9efa5f7ea36e2ba9c07f3f60
DunaevaSvetlana <solnste1993@mail.ru>
2020-04-28 23:15:14 +0400

    Initial commit

---
+++ src/main/java/Massiv.java
@@ -0,0 +1,13 @@
+public class Massiv {
+    public static void main(String[] args) {
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i+1;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers["+ i +"] = " + numbers[i]);
+        }
+    }
+}
